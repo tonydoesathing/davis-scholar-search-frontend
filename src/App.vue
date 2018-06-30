d88a2d4cc850510486550c5917ba3ce07ebcbe3c
tonydoesathing <40305786+tonydoesathing@users.noreply.github.com>
2018-06-30 12:49:56 -0400

    Figuring out Vue
    Included basic component and stuff... Next step is to integrate router and then transition old code

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img width="25%" src="./assets/logo.png">
-    <HelloWorld/>
+    <PageHeader title="Meow"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import PageHeader from "./components/PageHeader";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    PageHeader
   }
 };
 </script>
@@ -21,8 +20,6 @@ export default {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
